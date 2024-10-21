1382dd09105fe17679bb4509bb38f660af4007bb
Hayk8637 <62342210+Hayk8637@users.noreply.github.com>
2024-10-21 20:13:33 +0400

    ooo

--- src/components/managment/establishments/modals/qrOrLink/qrOrLink.tsx
+++ src/components/managment/establishments/modals/qrOrLink/qrOrLink.tsx
@@ -1,34 +1,59 @@
-import { Button, Modal, notification, QRCode } from 'antd'
-import React from 'react'
-import styles from '../../style.module.css'
+import { Button, Modal, notification, QRCode } from 'antd';
+import React, { useRef } from 'react';
+import styles from '../../style.module.css';
+import * as htmlToImage from 'html-to-image';
+
 interface IQrOrLinkProps {
-    isModalVisible: boolean;
-    onCancel: () => void;
-    selectedEstablishmentId: any;
-    userId: any;
+  isModalVisible: boolean;
+  onCancel: () => void;
+  selectedEstablishmentId: any;
+  userId: any;
 }
 
-const QrOrLink:React.FC<IQrOrLinkProps> = ({isModalVisible , onCancel , selectedEstablishmentId , userId}) => {
-    const handleCopyLink = () => {
-        const linkToCopy = `https://menu.menubyqr.com/${userId}}/${selectedEstablishmentId}`;
-        navigator.clipboard.writeText(linkToCopy)
-          .then(() => {
-            notification.success({ message: 'Link copied to clipboard!' });
-          })
-          .catch((error) => {
-            console.error('Failed to copy the link: ', error);
-            notification.error({ message: 'Failed to copy the link', description: error.message });
-          });
-      };
+const QrOrLink: React.FC<IQrOrLinkProps> = ({ isModalVisible, onCancel, selectedEstablishmentId, userId }) => {
+  const qrRef = useRef<HTMLDivElement>(null);
+
+  const handleCopyLink = () => {
+    const linkToCopy = `https://menu.menubyqr.com/${userId}/${selectedEstablishmentId}`;
+    navigator.clipboard.writeText(linkToCopy)
+      .then(() => {
+        notification.success({ message: 'Link copied to clipboard!' });
+      })
+      .catch((error) => {
+        console.error('Failed to copy the link: ', error);
+        notification.error({ message: 'Failed to copy the link', description: error.message });
+      });
+  };
+
+  const handleDownloadQrCode = () => {
+    if (qrRef.current) {
+      htmlToImage.toPng(qrRef.current)
+        .then((dataUrl) => {
+          const link = document.createElement('a');
+          link.href = dataUrl;
+          link.download = 'QRCode.png';
+          link.click();
+        })
+        .catch((error) => {
+          console.error('Failed to download QR code: ', error);
+          notification.error({ message: 'Failed to download QR code', description: error.message });
+        });
+    }
+  };
+
   return (
     <Modal title="QR or Link" open={isModalVisible} onCancel={onCancel} footer={null}>
       {selectedEstablishmentId && (
         <div className={styles.qrlink}>
-          <div className={styles.qr}>
+          <div className={styles.qr} ref={qrRef}>
             <QRCode
               className={styles.qrcode}
               errorLevel="H"
-              value={`https://menu.menubyqr.com/${userId}/${selectedEstablishmentId}`}/>
+              value={`https://menu.menubyqr.com/${userId}/${selectedEstablishmentId}`}
+            />
+            <Button type="default" className={styles.qrlinkbutton} onClick={handleDownloadQrCode} style={{ marginLeft: '8px' , marginTop: '10px' }}>
+              Download QR Code
+            </Button>
           </div>
           <div className={styles.link}>
             <p>QR Link:</p>
@@ -36,13 +61,14 @@ const QrOrLink:React.FC<IQrOrLinkProps> = ({isModalVisible , onCancel , selected
               {`https://menu.menubyqr.com/${userId}/${selectedEstablishmentId}`}
             </a>
             <Button type="primary" className={styles.qrlinkbutton} onClick={handleCopyLink}>
-              copy menu link
+              Copy Menu Link
             </Button>
+            
           </div>
         </div>
       )}
     </Modal>
-  )
-}
+  );
+};
 
-export default QrOrLink
+export default QrOrLink;
